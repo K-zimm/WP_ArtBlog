27abd0997ea7c62f03f2b8bce93006bb1adc60c9
Kyle Zimmer <kylezimmer2@gmail.com>
2018-03-17 09:36:55 -0500

    First commit of theme for MorganZimmer.com

---
+++ morganzimmer/template-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Template Name: Sample Page
+ * 
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<?php the_title('<h1>', '</h1>'); ?>
+
+<?php the_post_thumbnail('medium'); ?>
+
+<?php get_footer(); ?>
